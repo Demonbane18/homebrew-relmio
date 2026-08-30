f6dbeed93b51aeba42b8d28559267a8595f31b38
John Paul Fusin <butterncreamz@gmail.com>
2026-08-31 03:11:09 +0800

    Update Relmio to 0.9.1

--- Formula/relmio.rb
+++ Formula/relmio.rb
@@ -1,8 +1,8 @@
 class Relmio < Formula
   desc "Set up a private OpenAI-compatible endpoint for self-hosted n8n"
   homepage "https://github.com/Demonbane18/relmio"
-  url "https://registry.npmjs.org/relmio/-/relmio-0.9.0.tgz"
-  sha256 "7bd80609e3ef45663d56fa17aba95c264891b304efe42c1c32613682ea0998b2"
+  url "https://registry.npmjs.org/relmio/-/relmio-0.9.1.tgz"
+  sha256 "a8e0e89ade63ab1827287ca0d54524b2ef28e401ebe2efa705a8352366717d80"
   license "Apache-2.0"
 
   depends_on "python" => :build
